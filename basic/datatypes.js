2b965756914333066c6c1be543c565411fbd8ffa
ayushman1309 <pathakayush1309@gmail.com>
2025-03-07 19:59:52 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,29 @@
+"Use Strict";
+
+//alert(3 + 3); //we are using node.js, so we can't use alert function
+
+// console.log(3 + 3); //code redability is better in console.log
+// console.log("XYZ");
+
+// console.log("3" + 3);
+
+let name = "John";
+let age = 25;
+
+let isLoggedIn = true;
+
+// number => 2^53 - 1
+// bigint => 2^53
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique identifiers
+
+
+// object => key-value pairs
+
+console.log(typeof "John");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Symbol("id"));
