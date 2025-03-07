4cb4fccdc3bfd11622c1d1033bec018197594dee
ayushman1309 <pathakayush1309@gmail.com>
2025-03-07 20:45:31 +0000

    datatypes

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -7,10 +7,10 @@
 
 // console.log("3" + 3);
 
-let name = "John";
-let age = 25;
+// // let name = "John";
+// let age = 25;
 
-let isLoggedIn = true;
+// let isLoggedIn = true;
 
 // number => 2^53 - 1
 // bigint => 2^53
@@ -23,7 +23,81 @@ let isLoggedIn = true;
 
 // object => key-value pairs
 
-console.log(typeof "John");
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof Symbol("id"));
+// console.log(typeof "John");
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof Symbol("id"));
+
+// let score = "100abc";
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+// let isLoggedIn = "hjbfkjfgk";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(typeof someNumber);
+
+// let value = 3
+// let negValue = -value;
+// console.log(negValue);
+
+// let str1 = "Hello";
+// let str2 = "World";
+// let str3 = str1 + " " + str2;
+// console.log(str3);
+
+// console.log(str1 + " " + 3);
+
+// console.log("1" +2);
+
+// console.log("1" + 2 + 3);//in this case 1 is string and its initial value is string so it will concatenate with 2 and then 3
+
+// console.log(1 + 2 + "3");//in this case 1 is number and its initial value is number so it will add with 2 and then concatenate with 3 because 3 is a string
+// console.log(1 + "2" + 3);
+// console.log("1" + 2 + "3");
+
+// console.log(3 + 4 - 5 * 2 / 2);
+// console.log((3 + 4) - (5 * 2) / 2);
+// console.log(3 + 4 - 5 * (2 / 2));
+
+// let num1, num2 ,num3;
+// num1 = num2 = num3 = 2+2;
+// console.log(num1);
+// console.log(num2);  
+// console.log(num3);
+
+
+//POSTFIX,PREFIX
+// let x = 100
+
+// const y = x++;
+// console.log(x,y);//first x will be assigned to y and then x will be incremented
+
+// // ++gameCounter;
+
+// --gameCounter;
+// console.log(gameCounter);
+
+// console.log(3 > 2);
+// console.log(3 < 2);
+
+console.log("02" > 1);
